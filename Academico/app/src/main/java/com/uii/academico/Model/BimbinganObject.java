1bb2240fcd09ec74e2a41e56c29bef5c08e3b1ee
hrithikv <35966198+hrithikv@users.noreply.github.com>
2020-08-28 00:58:21 -0400

    chatting and push notification using firebase cloud messaging

---
+++ Academico/app/src/main/java/com/uii/academico/Model/BimbinganObject.java
@@ -0,0 +1,31 @@
+package com.uii.academico.Model;
+
+public class BimbinganObject {
+
+    private String nama, id, urlFoto;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getUrlFoto() {
+        return urlFoto;
+    }
+
+    public void setUrlFoto(String urlFoto) {
+        this.urlFoto = urlFoto;
+    }
+}
