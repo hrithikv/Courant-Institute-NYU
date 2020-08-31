9995cdf6151bf6b33e1cf5e20f35ef0355c1e114
hrithikv <35966198+hrithikv@users.noreply.github.com>
2020-08-31 06:52:45 -0400

    chatting and push notification using firebase cloud messaging

--- Academico/app/src/main/java/com/uii/academico/FCM/FcmInstanceIDListenerService.java
+++ Academico/app/src/main/java/com/uii/academico/FCM/FcmInstanceIDListenerService.java
@@ -6,27 +6,19 @@ import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 import com.uii.academico.Utility.Utils;
 
-
-
-
 public class FcmInstanceIDListenerService extends FirebaseInstanceIdService {
 
     @Override
     public void onTokenRefresh() {
 
-        // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.e("TOKEN NYA : ", "Refreshed token: " + refreshedToken);
-        // TODO: Implement this method to send any registration to your app's servers.
         simpanRegistrasiToken(refreshedToken);
     }
 
     public void  simpanRegistrasiToken (String refreshedToken) {
 
-        // Token disimpan di Utils sharedpreference kemudian di ambil LoginActivity untuk post ke server academico
         if (Utils.sharedPreferences.contains("FcmToken")) {
-
-           // Gak melakukan apa-apa
 
         } else {
 
